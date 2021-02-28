24f52850ced48ff01ab3250cb72af3246176a8a4
KristaYanb <yanbeckova.kristina@yandex.ru>
2021-02-28 14:02:37 +0700

    new virtual function-Summa

--- matr_atd.h
+++ matr_atd.h
@@ -14,6 +14,7 @@ namespace simple_matrix
 		static matr* In(ifstream& ifst);
 		virtual void InData(ifstream& ifst) = 0; // ввод
 		virtual void Out(ofstream& ofst) = 0; // вывод
+		virtual int Summa() = 0;
 	};
 } // end simple_matrix namespace
 #endif
