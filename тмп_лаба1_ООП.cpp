560cbeb4f0c9df9e98d365b74ef379eaca5bc8e1
KristaYanb <yanbeckova.kristina@yandex.ru>
2021-02-28 14:39:01 +0700

    new function-sort

--- тмп_лаба1_ООП.cpp
+++ тмп_лаба1_ООП.cpp
@@ -21,7 +21,9 @@ int main(int argc, char* argv[])
 
 	c.In(ifst);
 	ofst << "Filled container. " << endl;
-
+	
+	ofst << "Sorted container. " << endl;
+	c.sort();
 	c.Out(ofst);
 	c.Clear();
 	ofst << "Empty container. " << endl;
